015e69cc17c680673e64d8264f933090604ce70c
ci-adapter <121284078+ci-adapter@users.noreply.github.com>
2022-12-26 11:32:42 +0300

    Update math.go

--- internal/math/math.go
+++ internal/math/math.go
@@ -6,21 +6,18 @@ import (
 
 func Sinus(input float64) (output float64) {
 	// Insert your code here
-	return math.Sin(input)
 
-	// return 0.0
+	return 0.0
 }
 
 func Cosinus(input float64) (output float64) {
 	// Insert your code here
-	return math.Cos(input)
 
-	// return 0.0
+	return 0.0
 }
 
 func Tangens(input float64) (output float64) {
 	// Insert your code here
-	return math.Tan(input)
 
-	// return 0.0
+	return 0.0
 }
